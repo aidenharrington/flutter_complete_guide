a4228cdf77930a8784a6bdff78d2cd3c65d71c52
Aiden Harrington <aidenkharrington@gmail.com>
2021-04-12 08:58:48 -0700

    5:23:45

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   // const is compile time constant
   // implicitly runtime constant as a result
   // you can also add const in front of value instead of variable
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'What\'s your favourite colour?',
       'answers': ['Black', 'Red', 'Green', 'White'],
@@ -37,8 +37,6 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _questionIndex += 1;
     });
-
-    if (_questionIndex >= questions.length) {}
   }
 
   @override
@@ -48,15 +46,12 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex % 3]['questionText']),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions)
+            : Result(),
       ),
     );
   }
